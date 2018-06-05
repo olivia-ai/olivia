da6bb2dda16cce63f26d205698e3643e0b2dfe6c
AnanaGame <gameanana@gmail.com>
2018-06-05 21:48:10 +0200

    Organize the Intents with words, classes and documents

--- analysis/intents.go
+++ analysis/intents.go
@@ -3,7 +3,9 @@ package analysis
 import (
 	"encoding/json"
 	"fmt"
+	"github.com/neurosnap/sentences"
 	"io/ioutil"
+	"strings"
 )
 
 type Intent struct {
@@ -11,6 +13,21 @@ type Intent struct {
 	Patterns  []string `json:"patterns"`
 	Responses []string `json:"responses"`
 	Context   string   `json:"context"`
+}
+
+type Document struct {
+	Words []string
+	Tag   string
+}
+
+func SliceContains(slice []string, text string) bool {
+	for _, item := range slice {
+		if text == item {
+			return true
+		}
+	}
+
+	return false
 }
 
 // Return the intents json file's content
@@ -29,4 +46,43 @@ func Serialize() []Intent {
 	json.Unmarshal(Read(), &intents)
 
 	return intents
+}
+
+func Organize() (words, classes []string, documents []Document) {
+	for _, intent := range Serialize() {
+		for _, response := range intent.Responses {
+			tokenizer := sentences.NewWordTokenizer(nil)
+			tokens := tokenizer.Tokenize(response, true)
+
+			// Initialize empty string array of tokens length
+			var tokenizedWords []string
+
+			// Get the string token and add it to tokenizedWords
+			for _, tokenizedWord := range tokens {
+				word := strings.ToLower(tokenizedWord.Tok)
+
+				if word != "?" && word != "-" {
+					tokenizedWords = append(tokenizedWords, word)
+				}
+			}
+
+			// Add each word to response
+			for _, word := range tokenizedWords {
+				words = append(words, word)
+			}
+
+			// Add a new document
+			documents = append(documents, Document{
+				tokenizedWords,
+				intent.Tag,
+			})
+
+			// Add the intent tag to class if it doesn't exists
+			if SliceContains(classes, intent.Tag) {
+				classes = append(classes, intent.Tag)
+			}
+		}
+	}
+
+	return words, classes, documents
 }
