31d86c02d6d1aee0682634ea2248bb6361a04479
AnanaGame <gameanana@gmail.com>
2018-06-21 00:24:49 +0200

    Testing 8080 HTTP

--- main.go
+++ main.go
@@ -1,7 +1,11 @@
 package main
 
-import "github.com/ananagame/Olivia/supports"
+import (
+	"github.com/ananagame/Olivia/supports"
+	"net/http"
+)
 
 func main() {
+	http.ListenAndServe(":8080", nil)
 	supports.ChooseSupport()
 }
