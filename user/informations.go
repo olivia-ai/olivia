27a597404c082b42afcf8327389f01ab1906efda
AnanaGame <gameanana@gmail.com>
2019-06-27 22:39:31 +0200

    Get the user informations if there are changes

--- user/informations.go
+++ user/informations.go
@@ -1,5 +1,7 @@
 package user
 
+import "github.com/olivia-ai/olivia/util"
+
 type Informations struct {
 	Name string `json:"name"`
 }
@@ -15,4 +17,16 @@ func ChangeUserInformations(token string, changer func (Informations) Informatio
 	userInformations[token] = changer(userInformations[token])
 	// Add the user's token to userInformationsUpdates to
 	userInformationsUpdates = append(userInformationsUpdates, token)
+}
+
+// GetUserInformations returns the informations of a user with his token, returns an empty information
+// if there were no modifications since.
+func GetUserInformations(token string) Informations {
+	if !util.Contains(userInformationsUpdates, token) {
+		return Informations{}
+	}
+
+	userInformationsUpdates = util.Remove(userInformationsUpdates, token)
+
+	return userInformations[token]
 }
