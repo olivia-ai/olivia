c2bb44dcbe3a957762443ea907e74d5064387d22
AnanaGame <gameanana@gmail.com>
2019-07-02 13:13:09 +0200

    Add the names dataset and the serializer

---
+++ language/names.go
@@ -0,0 +1,32 @@
+package language
+
+import (
+	"github.com/olivia-ai/olivia/util"
+	"strings"
+)
+
+var names = SerializeNames()
+
+func SerializeNames() (names []string) {
+	namesFile := string(util.ReadFile("res/names.txt"))
+
+	// Iterate each line of the file
+	for _, name := range strings.Split(strings.TrimSuffix(namesFile, "\n"), "\n") {
+		names = append(names, name)
+	}
+
+	return
+}
+
+// FindName returns a name found in the given sentence or an empty string if no name has been found
+func FindName(sentence string) string {
+	for _, name := range names {
+		if !strings.Contains(sentence, name) {
+			continue
+		}
+
+		return name
+	}
+
+	return ""
+}
