09f3b6bc37b513b76bb8a73a27230f2c1a6ff934
Christian Muehlhaeuser <muesli@gmail.com>
2019-07-19 05:22:41 +0200

    Removed unnecessary float conversion
    x is already a float.

--- neuralnet/math.go
+++ neuralnet/math.go
@@ -3,7 +3,7 @@ package neuralnet
 import "math"
 
 func Sigmoid(x float64) float64 {
-	return 1.0 / (1.0 + math.Pow(math.E, -float64(x)))
+	return 1.0 / (1.0 + math.Pow(math.E, -x))
 }
 
 func SigmoidDerivative(x float64) float64 {
