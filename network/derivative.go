044df4719ec4498bb7157fd8656b0e052b78b2f4
AnanaGame <gameanana@gmail.com>
2020-03-02 13:45:43 +0100

    Create a file where the derivatives are computed

---
+++ network/derivative.go
@@ -0,0 +1,67 @@
+package network
+
+type Derivative struct {
+	Delta      Matrix
+	Adjustment Matrix
+}
+
+// ComputeLastLayerDerivatives returns the derivatives of the last layer L
+func (network Network) ComputeLastLayerDerivatives() Derivative {
+	l := len(network.Layers) - 1
+	lastLayer := network.Layers[l]
+
+	// Compute derivative for the last layer of weights and biases
+	cost := Difference(network.Output, lastLayer)
+	sigmoidDerivative := Multiplication(lastLayer, ApplyFunction(lastLayer, SubstractOne))
+
+	// Compute delta and the weights' adjustment
+	delta := Multiplication(
+		ApplyFunction(cost, MultiplyByTwo),
+		sigmoidDerivative,
+	)
+	weights := DotProduct(Transpose(network.Layers[l-1]), delta)
+
+	return Derivative{
+		Delta:      delta,
+		Adjustment: weights,
+	}
+}
+
+// ComputeDerivatives returns the derivatives of a specific layer l defined by i
+func (network Network) ComputeDerivatives(i int, derivatives []Derivative) Derivative {
+	l := len(network.Layers) - 2 - i
+
+	// Compute derivative for the layer of weights and biases
+	delta := Multiplication(
+		DotProduct(
+			derivatives[i].Delta,
+			Transpose(network.Weights[l]),
+		),
+		Multiplication(
+			network.Layers[l],
+			ApplyFunction(network.Layers[l], SubstractOne),
+		),
+	)
+	weights := DotProduct(Transpose(network.Layers[l-1]), delta)
+
+	return Derivative{
+		Delta:      delta,
+		Adjustment: weights,
+	}
+}
+
+// Adjust make the adjusts
+func (network Network) Adjust(derivatives []Derivative) {
+	for i, derivative := range derivatives {
+		l := len(derivatives) - i
+
+		network.Weights[l-1] = Sum(
+			network.Weights[l-1],
+			ApplyRate(derivative.Adjustment, network.Rate),
+		)
+		network.Biases[l-1] = Sum(
+			network.Biases[l-1],
+			ApplyRate(derivative.Delta, network.Rate),
+		)
+	}
+}
