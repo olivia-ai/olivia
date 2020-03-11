c3a1e074312c11f5c5ff4a1d7b14299b79182575
Hugo Lageneste <hugo.lageneste@pm.me>
2020-03-11 19:21:29 +0100

    Update sentence.Tokenize to sentence.Stem to avoid misinterpretation

--- analysis/format.go
+++ analysis/format.go
@@ -51,7 +51,7 @@ func (sentence Sentence) WordsBag(words []string) (bag []float64) {
 	for _, word := range words {
 		// Append 1 if the patternWords contains the actual word, else 0
 		var valueToAppend float64 = 0
-		if util.Contains(sentence.Tokenize(), word) {
+		if util.Contains(sentence.Stem(), word) {
 			valueToAppend = 1
 		}
 
