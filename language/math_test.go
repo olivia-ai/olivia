8c795f25c0b876e6a822b1df329b0747bad76c42
Hugo Lageneste <hugo.lageneste@pm.me>
2020-03-28 22:41:53 +0100

    Create tests for language/math

---
+++ language/math_test.go
@@ -0,0 +1,23 @@
+package language
+
+import "testing"
+
+func TestFindMathOperation(t *testing.T) {
+	sentence := "Calculate cos(0.5) * 5.3 please"
+	excepted := "cos(0.5) * 5.3"
+	operation := FindMathOperation(sentence)
+
+	if operation != excepted {
+		t.Errorf("FindMathOperation() failed, excepted %s got %s.", excepted, operation)
+	}
+}
+
+func TestFindNumberOfDecimals(t *testing.T) {
+	sentence := "Calculate cos(0.5) * 5.3 with 8 decimals please"
+	excepted := 8
+	decimals := FindNumberOfDecimals(sentence)
+
+	if decimals != excepted {
+		t.Errorf("FindNumberOfDecimals() failed, excepted %d got %d.", excepted, decimals)
+	}
+}
