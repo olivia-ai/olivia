544d68706f020a62c70df8dbfcbbfeff9ea4ecb0
Hugo Lageneste <hugo@olivia-ai.org>
2020-03-29 13:51:29 +0200

    Create the date/DayOfWeek rule

--- language/date/rules.go
+++ language/date/rules.go
@@ -6,6 +6,18 @@ import (
 	"time"
 )
 
+const day = time.Hour * 24
+
+var daysOfWeek = map[string]time.Weekday{
+	"monday":    time.Monday,
+	"tuesday":   time.Tuesday,
+	"wednesday": time.Wednesday,
+	"thursday":  time.Thursday,
+	"friday":    time.Friday,
+	"saturday":  time.Saturday,
+	"sunday":    time.Sunday,
+}
+
 func init() {
 	// Register the rules
 	RegisterRule(RuleTomorrow)
@@ -22,7 +34,6 @@ func RuleTomorrow(sentence string) (result time.Time) {
 		return time.Time{}
 	}
 
-	day := time.Hour * 24
 	result = time.Now().Add(day)
 
 	// If the date contains "after", we add 24 hours to tomorrow's date
@@ -31,4 +42,41 @@ func RuleTomorrow(sentence string) (result time.Time) {
 	}
 
 	return
+}
+
+// RuleDayOfWeek checks for the days of the week and the keyword "next" in the given sentence,
+// then it returns the date parsed.
+func RuleDayOfWeek(sentence string) time.Time {
+	dayOfWeekRegex := regexp.MustCompile(`((next )?(monday|tuesday|wednesday|thursday|friday|saturday|sunday))`)
+	date := dayOfWeekRegex.FindString(sentence)
+
+	// Returns an empty date struct if no date has been found
+	if date == "" {
+		return time.Time{}
+	}
+
+	var foundDayOfWeek int
+	// Find the integer value of the found day of the week
+	for _, dayOfWeek := range daysOfWeek {
+		if strings.Contains(date, dayOfWeek.String()) {
+			foundDayOfWeek = int(dayOfWeek)
+		}
+	}
+
+	currentDay := int(time.Now().Weekday())
+	// Calculate the date of the found day
+	calculatedDate := currentDay - foundDayOfWeek
+
+	// If the day is already passed in the current week, then we add another week to the count
+	if calculatedDate < 0 {
+		calculatedDate += 7
+	}
+
+	// If there is "next" in the sentence, then we add another week
+	if strings.Contains(date, "next") {
+		calculatedDate += 7
+	}
+
+	// Then add the calculated number of day to the actual date
+	return time.Now().Add(day * time.Duration(calculatedDate))
 }
