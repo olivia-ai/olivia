c810c8a77f0e72ddac279dc678380329ca0db71c
Hugo Lageneste <hugo@olivia-ai.org>
2020-04-09 15:06:19 +0200

    Begin to create the authentication system for the API

---
+++ dashboard/authentication.go
@@ -0,0 +1,76 @@
+package dashboard
+
+import (
+	"encoding/json"
+	"os"
+
+	"github.com/olivia-ai/olivia/util"
+	"golang.org/x/crypto/bcrypt"
+)
+
+type AuthorizedUser struct {
+	Token string `json:"token"`
+}
+
+// HashToken returns the hash of the token using bcrypt
+func HashToken(token string) (string, error) {
+	bytes, err := bcrypt.GenerateFromPassword([]byte(token), bcrypt.DefaultCost)
+	return string(bytes), err
+}
+
+// CompareHash compares a hash with a token and returns the boolean
+func CompareHashAndToken(hash, token string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(token))
+	return err == nil
+}
+
+// InitializeFile creates the default authentication file with a first given token
+func InitializeFile(file, token string) {
+	// Create the data
+	hash, _ := HashToken(token)
+	data := []AuthorizedUser{
+		{Token: hash},
+	}
+
+	// Create the file
+	outF, err := os.OpenFile(file, os.O_CREATE|os.O_RDWR, 0777)
+	if err != nil {
+		panic("Failed to create " + file + ".")
+	}
+	defer outF.Close()
+
+	// Encode the json data
+	encoder := json.NewEncoder(outF)
+	err = encoder.Encode(data)
+	if err != nil {
+		panic(err)
+	}
+}
+
+// IsAuthenticated checks if the given token is contained in the authentication file,
+// if not it creates a file with the given hashed token inside.
+func IsAuthenticated(token string) bool {
+	file := "../res/api/authentication.json"
+
+	_, err := os.Open(file)
+	// Checks if the authentication file exists
+	if err == nil {
+		// Parse the data contained in the authentication file
+		var data []AuthorizedUser
+		json.Unmarshal(util.ReadFile(file), &data)
+
+		// Iterate the registered tokens to see if the given token is authorized
+		for _, o := range data {
+			// If the token is valid then returns true
+			if CompareHashAndToken(o.Token, token) {
+				return true
+			}
+		}
+
+		return false
+	}
+
+	// Creates the authentication file
+	InitializeFile(file, token)
+	return true
+}
