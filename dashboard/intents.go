b3c67b9da9b2f8e98d1a0b8f7982b31c1d23643e
Hugo Lageneste <hugo@olivia-ai.org>
2020-04-09 20:45:54 +0200

    Create the route to get all the intents

--- dashboard/intents.go
+++ dashboard/intents.go
@@ -1,1 +1,13 @@
 package dashboard
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/olivia-ai/olivia/analysis"
+)
+
+func GetIntents(w http.ResponseWriter, r *http.Request) {
+	intents := analysis.SerializeIntents("res/intents.json")
+	json.NewEncoder(w).Encode(intents)
+}
--- server/serve.go
+++ server/serve.go
@@ -38,6 +38,7 @@ func Serve(_neuralNetwork network.Network, port, _intentsPath string) {
 	// Serve the API
 	router.HandleFunc("/api/dashboard", GetDashboardData).Methods("GET")
 	router.HandleFunc("/api/authenticate", dashboard.GetAuthentication).Methods("GET")
+	router.HandleFunc("/api/intents", dashboard.GetIntents).Methods("GET")
 
 	magenta := color.FgMagenta.Render
 	fmt.Printf("\nServer listening on the port %s...\n", magenta(port))
