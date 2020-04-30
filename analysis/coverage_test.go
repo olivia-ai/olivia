f09e5e63c4eede6d6fa100d8f709553d046c3987
Hugo Lageneste <hugo.lageneste@pm.me>
2020-04-30 16:53:13 +0200

    Create the intents coverage method and add tests for coverage

---
+++ analysis/coverage_test.go
@@ -0,0 +1,21 @@
+package analysis
+
+import (
+	"testing"
+)
+
+func TestGetModuleCoverage(t *testing.T) {
+	notCovered, coverage := GetModuleCoverage("en")
+
+	if len(notCovered) != 0 || coverage != 100 {
+		t.Errorf("GetModuleCoverage() failed.")
+	}
+}
+
+func TestGetIntentCoverage(t *testing.T) {
+	notCovered, coverage := GetIntentCoverage("en")
+
+	if len(notCovered) != 0 || coverage != 100 {
+		t.Errorf("GetIntentCoverage() failed.")
+	}
+}
