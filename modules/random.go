295a7a79c4dc854f2f6c57a18097d4a588bf2914
Hariharan <cvhariharan@protonmail.com>
2020-05-29 13:47:50 +0530

    Update random module to support range, fixes #79

--- modules/random.go
+++ modules/random.go
@@ -4,6 +4,9 @@ import (
 	"fmt"
 	"math/rand"
 	"strconv"
+
+	"github.com/olivia-ai/olivia/language"
+	"github.com/olivia-ai/olivia/util"
 )
 
 // RandomTag is the intent tag for its module
@@ -11,6 +14,17 @@ var RandomTag = "random number"
 
 // RandomNumberReplacer replaces the pattern contained inside the response by a random number.
 // See modules/modules.go#Module.Replacer() for more details.
-func RandomNumberReplacer(_, _, response, _ string) (string, string) {
-	return RandomTag, fmt.Sprintf(response, strconv.Itoa(rand.Intn(100)))
+func RandomNumberReplacer(locale, entry, response, _ string) (string, string) {
+	limitArr, err := language.FindRangeLimits(locale, entry)
+	if err != nil {
+		if limitArr != nil {
+			return RandomTag, fmt.Sprintf(response, strconv.Itoa(rand.Intn(100)))
+		}
+		responseTag := "no random range"
+		return responseTag, util.GetMessage(locale, responseTag)
+	}
+	min := limitArr[0]
+	max := limitArr[1]
+	randNum := rand.Intn((max - min)) + min
+	return RandomTag, fmt.Sprintf(response, strconv.Itoa(randNum))
 }
