5331fa816b9859df1834053c4ea7ed8739dd3a6a
Hariharan <cvhariharan@protonmail.com>
2020-05-29 14:15:01 +0530

    Improve readability

--- language/random.go
+++ language/random.go
@@ -9,24 +9,28 @@ import (
 
 var decimal = "\\b\\d+([\\.,]\\d+)?"
 
+// FindRangeLimits finds the range for random numbers and returns a sorted integer array
 func FindRangeLimits(local, entry string) ([]int, error) {
 	decimalsRegex := regexp.MustCompile(decimal)
 	limitStrArr := decimalsRegex.FindAllString(entry, 2)
 	limitArr := make([]int, 0)
-	if limitStrArr != nil {
-		if len(limitStrArr) != 2 {
-			return nil, errors.New("Need 2 numbers, a lower and upper limit")
-		}
-		for _, v := range limitStrArr {
-			num, err := strconv.Atoi(v)
-			if err != nil {
-				return nil, errors.New("Non integer range")
-			}
-			limitArr = append(limitArr, num)
-		}
-	} else {
+
+	if limitStrArr == nil {
 		return make([]int, 0), errors.New("No range")
 	}
+
+	if len(limitStrArr) != 2 {
+		return nil, errors.New("Need 2 numbers, a lower and upper limit")
+	}
+
+	for _, v := range limitStrArr {
+		num, err := strconv.Atoi(v)
+		if err != nil {
+			return nil, errors.New("Non integer range")
+		}
+		limitArr = append(limitArr, num)
+	}
+
 	sort.Ints(limitArr)
 	return limitArr, nil
 }
