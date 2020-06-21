f11b4443455f4d845f7b5f8ce966f84e14be5bd2
Michael Loukeris <loukerismichalis@gmail.com>
2020-06-21 14:08:39 +0300

    (translations): Add initial Greek translations

--- language/math.go
+++ language/math.go
@@ -16,6 +16,8 @@ var MathDecimals = map[string]string{
 	"it": `(\d+( |-)decimale(s)?)|(numero (di )?decimale(s)? (è )?\d+)`,
 	"tr": `(\d+( |-)desimal(s)?)|(numara (dan )?desimal(s)? (mı )?\d+)`,
 	"nl": `(\d+( |-)decimal(en)?)|(nummer (van )?decimal(en)? (is )?\d+)`,
+	"el": `(\d+( |-)δεκαδικ(ό|ά)?)|(αριθμός (από )?δεκαδικ(ό|ά)? (είναι )?\d+)`,
+
 }
 
 // FindMathOperation finds a math operation in a string an returns it
