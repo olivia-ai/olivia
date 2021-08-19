2e6f7bb62ccc1335a4d72f86e6323f42325f695c
Hugo Lageneste <hugo.lageneste@pm.me>
2021-08-19 19:59:55 +0200

    Revert exportation of math methods in model

--- model/math.go
+++ model/math.go
@@ -2,17 +2,17 @@ package model
 
 import "math"
 
-// Sigmoid is the activation function
-func Sigmoid(x float64) float64 {
+// sigmoid is the chosen activation function
+func sigmoid(x float64) float64 {
 	return 1 / (1 + math.Exp(-x))
 }
 
 // MultipliesByTwo takes a float and returns the float multiplied by two
-func MultipliesByTwo(x float64) float64 {
+func multipliesByTwo(x float64) float64 {
 	return 2 * x
 }
 
 // SubtractsOne takes a float and returns the float subtracted by one
-func SubtractsOne(x float64) float64 {
+func subtractsOne(x float64) float64 {
 	return x - 1
 }
--- model/math_test.go
+++ model/math_test.go
@@ -5,21 +5,21 @@ import (
 )
 
 func TestSigmoid(t *testing.T) {
-	result := Sigmoid(10)
+	result := sigmoid(10)
 	if result != 0.9999546021312976 {
 		t.Errorf("Sigmoid() failed.")
 	}
 }
 
 func TestMultipliesByTwo(t *testing.T) {
-	result := MultipliesByTwo(2)
+	result := multipliesByTwo(2)
 	if result != 4 {
 		t.Errorf("MultipliesByTwo() failed.")
 	}
 }
 
 func TestSubtractsOne(t *testing.T) {
-	result := SubtractsOne(21)
+	result := subtractsOne(21)
 	if result != 20 {
 		t.Errorf("SubtractsOne() failed.")
 	}
