6c206cd206aca6b958d387461ae3dbc936ee2f90
Hugo Lageneste <hugo.lageneste@pm.me>
2021-08-29 17:14:30 +0200

    Add the decoder's part in the feedforward of the seq2seq

--- model/nn_test.go
+++ model/nn_test.go
@@ -15,7 +15,7 @@ func TestCreateNeuralNetwork(t *testing.T) {
 	}
 }
 
-func TestFeedForward(t *testing.T) {
+func TestNNFeedForward(t *testing.T) {
 	nn := CreateNeuralNetwork(0.5, 1, 1)
 	rand.Seed(1)
 	output := nn.FeedForward([]float64{0.5})
