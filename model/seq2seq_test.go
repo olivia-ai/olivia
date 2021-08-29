174b22b085ead89f36fa95da6c11da473d77467d
Hugo Lageneste <hugo.lageneste@pm.me>
2021-08-29 22:33:57 +0200

    Implement the vocabulary size in the seq2seq structure

--- model/seq2seq_test.go
+++ model/seq2seq_test.go
@@ -3,7 +3,7 @@ package model
 import "testing"
 
 func TestS2SFeedForward(t *testing.T) {
-	model := CreateSeq2Seq(10, 0.25, []int{})
+	model := CreateSeq2Seq(10, 0.25)
 	empty := make([]float64, 10)
 	model.FeedForward([][]float64{empty, empty, empty})
 }
