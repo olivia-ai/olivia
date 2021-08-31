5cbee852126416851900a0538b9ae9965c23f9d7
Hugo Lageneste <hugo.lageneste@pm.me>
2021-08-31 19:53:58 +0200

    Test the vocabulary util of the NLP

--- model/seq2seq.go
+++ model/seq2seq.go
@@ -1,6 +1,10 @@
 package model
 
-import "reflect"
+import (
+	"reflect"
+
+	nlpe "github.com/olivia-ai/olivia/nlp/embeddings"
+)
 
 // Seq2Seq contains the stucture of a sequence to sequence model including
 // the encoder and decoder recurrent neural networks.
@@ -8,18 +12,22 @@ type Seq2Seq struct {
 	Encoder NN
 	Decoder NN
 	VocabularySize int
+	EOS []float64
+	BOS []float64
 }
 
 // CreateSeq2Seq creates and returns a new sequence to sequence (seq2seq) model.
 func CreateSeq2Seq(vocabularySize int, learningRate float64, hiddenLayersNodes ...int) Seq2Seq {
-	// Use twice the size of the embedding size for the encoder because we need to take the input
-	// embedding and the previous one as input.
-	nn := CreateNeuralNetwork(learningRate, 2 * vocabularySize, vocabularySize, hiddenLayersNodes...)
-
 	return Seq2Seq{
-		Encoder: nn,
-		Decoder: nn,
+		// Use twice the size of the embedding size for the encoder because we need to take the input
+		// embedding and the previous one as input.
+		Encoder: CreateNeuralNetwork(learningRate, 2 * vocabularySize, vocabularySize, hiddenLayersNodes...),
+		Decoder: CreateNeuralNetwork(learningRate, 2 * vocabularySize, 2 * vocabularySize, hiddenLayersNodes...),
+
+		// Initialize helpers to access data on the vocabulary
 		VocabularySize: vocabularySize,
+		EOS: nlpe.GetEOS(vocabularySize),
+		BOS: nlpe.GetBOS(vocabularySize),
 	}
 }
 
@@ -27,27 +35,32 @@ func CreateSeq2Seq(vocabularySize int, learningRate float64, hiddenLayersNodes .
 // of the sequence to sequence model
 func (s2s *Seq2Seq) FeedForward(embeddings matrix) matrix {
 	hiddenStates := matrix{
+		// Initialize the hidden states with an empty embedding
 		make([]float64, len(embeddings[0])),
 	}
 	for i, embedding := range embeddings {
+		// Concatenate the input with the given embedding and the previous hidden state
 		input := append(embedding, hiddenStates[i]...)
 		hidden := s2s.Encoder.FeedForward(input)
 		hiddenStates = append(hiddenStates, hidden[0])
 	}
 
 	decoderHiddenStates := matrix{
+		// Begin with the last hidden state of the encoder
 		hiddenStates[len(hiddenStates)-1],
 	}
-	output := matrix{
-		// START token
-		make([]float64, len(embeddings[0])),
-	}
+	output := matrix{s2s.BOS}
 
-	// END token
-	for i := 0 ;reflect.DeepEqual(output[len(output)-1], make([]float64, len(embeddings[0]))); i++ {
+	for i := 0; !reflect.DeepEqual(s2s.EOS, output[len(output)-1]); i++ {
+		// Concatenate the previous output with the current hidden state
 		input := append(output[len(output)-1], decoderHiddenStates[i]...)
-		output = append(output, s2s.Decoder.FeedForward(input)[0])
+
+		decoderOutput := s2s.Decoder.FeedForward(input)[0]
+
+		// Split the decoder output in two equal parts for the word output and the hidden state
+		output = append(output, decoderOutput[0:s2s.VocabularySize])
+		decoderHiddenStates = append(decoderHiddenStates, decoderOutput[s2s.VocabularySize:])
 	}
 
-	return output[1:]
+	return output
 }
--- model/seq2seq_test.go
+++ model/seq2seq_test.go
@@ -1,9 +1,28 @@
 package model
 
-import "testing"
+import (
+	"fmt"
+	"strings"
+	"testing"
+
+	"github.com/olivia-ai/olivia/data"
+	"github.com/olivia-ai/olivia/nlp/embeddings"
+)
 
 func TestS2SFeedForward(t *testing.T) {
-	model := CreateSeq2Seq(10, 0.25)
-	empty := make([]float64, 10)
-	model.FeedForward([][]float64{empty, empty, empty})
+
+	s := strings.Fields("how are you?")
+	c := data.ReadCSVConversationalDataset("data/mock.csv")
+	voc := embeddings.EstablishVocabulary(c)
+	var input matrix
+	for _, word := range s {
+		input = append(
+			input, 
+			embeddings.GetLevenshteinEmbedding(voc, word),
+		)
+	}
+
+	model := CreateSeq2Seq(len(voc) + 2, 0.25)
+	fmt.Println(model.Decoder.Layers)
+	fmt.Println(model.FeedForward(input))
 }
